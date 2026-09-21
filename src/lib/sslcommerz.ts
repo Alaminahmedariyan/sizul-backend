d732e87c886edae479459a05eae545208460ea80
Alamin Ahmed <alaminahmedariyan2022@gmail.com>
2026-09-21 18:13:32 +0600

    fix: add DOM lib for fetch Response types

--- src/lib/sslcommerz.ts
+++
@@ -1,9 +0,0 @@
-import SSLCommerzPayment from "sslcommerz-lts";
-
-import config from "../app/config";
-
-export const sslcommerz = new SSLCommerzPayment(
-	config.sslcommerz.storeId ?? "",
-	config.sslcommerz.storePassword ?? "",
-	config.sslcommerz.isLive,
-);
